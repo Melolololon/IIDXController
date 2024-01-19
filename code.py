c8f8a0a4ca68ea525ac4f9cdbbf8650e08014c21
Melon <ichikawajun12917@icloud.com>
2024-01-19 18:47:38 +0900

    DP_モード切替実装中

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
-# SPDX-FileCopyrightText: 2021 Jeff Epler for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
+#  SPDX-FileCopyrightText: 2021 Jeff Epler for Adafruit Industries
+# 
+#  SPDX-License-Identifier: MIT
 
 import keypad
 import board
@@ -9,7 +9,7 @@ from adafruit_hid.keyboard import Keyboard, find_device
 from adafruit_hid.keycode import Keycode
 
 key_pins = [
-    board.GP2,
+    board.GP2,# 1P
     board.GP3,
     board.GP4,
     board.GP5,
@@ -20,6 +20,15 @@ key_pins = [
     board.GP10,
     board.GP11,
     board.GP12,
+    board.GP13,# 2P
+    board.GP14,
+    board.GP15,
+    board.GP18,
+    board.GP19,
+    board.GP20,
+    board.GP21,
+    board.GP22,
+    board.GP26,
 ]
 
 keys = keypad.Keys(key_pins, value_when_pressed=False, pull=True)
@@ -35,8 +44,8 @@ class BitmapKeyboard(Keyboard):
 
         self._keyboard_device = device
 
-        # report[0] modifiers
-        # report[1:16] regular key presses bitmask
+        #  report[0] modifiers
+        #  report[1:16] regular key presses bitmask
         self.report = bytearray(16)
 
         self.report_modifier = memoryview(self.report)[0:1]
@@ -45,7 +54,7 @@ class BitmapKeyboard(Keyboard):
     def _add_keycode_to_report(self, keycode):
         modifier = Keycode.modifier_bit(keycode)
         if modifier:
-            # Set bit for this modifier.
+            #  Set bit for this modifier.
             self.report_modifier[0] |= modifier
         else:
             self.report_bitmap[keycode >> 3] |= 1 << (keycode & 0x7)
@@ -53,7 +62,7 @@ class BitmapKeyboard(Keyboard):
     def _remove_keycode_from_report(self, keycode):
         modifier = Keycode.modifier_bit(keycode)
         if modifier:
-            # Set bit for this modifier.
+            #  Set bit for this modifier.
             self.report_modifier[0] &= ~modifier
         else:
             self.report_bitmap[keycode >> 3] &= ~(1 << (keycode & 0x7))
@@ -64,8 +73,6 @@ class BitmapKeyboard(Keyboard):
         self._keyboard_device.send_report(self.report)
 
  
-
-
 from board import *
 import rotaryio
 import usb_hid
@@ -75,46 +82,77 @@ import time
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard import Keycode
 
- 
-
-#キーコード https://docs.circuitpython.org/projects/hid/en/4.1.6/api.html
-keys1P = [Keycode.Q,Keycode.W,Keycode.E,Keycode.R,Keycode.T,Keycode.Y,Keycode.U,Keycode.ENTER,Keycode.BACKSPACE,Keycode.P,Keycode.L,Keycode.K,Keycode.J]
-scrUp1P = Keycode.LEFT_SHIFT
-scrDown1P = Keycode.CONTROL
+# キーコード https://docs.circuitpython.org/projects/hid/en/4.1.6/api.html
+keyCodes = [[Keycode.Q,Keycode.W,Keycode.E,Keycode.R,Keycode.T,Keycode.Y,Keycode.U,Keycode.ENTER,Keycode.BACKSPACE,Keycode.P,Keycode.L,Keycode.K,Keycode.J],
+            [Keycode.H,Keycode.G,Keycode.F,Keycode.D,Keycode.S,Keycode.A,Keycode.Z,Keycode.ENTER,Keycode.BACKSPACE,Keycode.X,Keycode.C,Keycode.C,Keycode.C]]
+scrUp = [Keycode.LEFT_SHIFT,Keycode.M]
+scrDown = [Keycode.CONTROL,Keycode.N]
 kbd = BitmapKeyboard(usb_hid.devices)
 
+# 除数
+DIVISOR = 1
+encoder = [rotaryio.IncrementalEncoder(GP0,GP1,DIVISOR),rotaryio.IncrementalEncoder(GP16,GP17,DIVISOR)]
+# 前フレームの位置(変化確認用)
+prePos = [0,0]
 
-encoder = rotaryio.IncrementalEncoder(GP0,GP1,1)
-prePos = 0
+# 皿が反応するまでにどのくらい回すかを設定する為の数値
+COUNT_MAX = 4
+# 以下がCOUNT_MAXを超えたら皿が反応する
+count = [0,0]
+preCount = [0,0]
 
-#皿が反応するまでにどのくらい回すかを設定する為の数値
-countMax = 4
-#以下がcountMaxを超えたら皿が反応する
-count = 0
-preCount = 0
+# 回したらどのくらい反応させたままにするか
+REACTION_COUNT_MAX = 1500
+# 以下がREACTION_COUNT_MAXを超えたら皿の反応がなくなる
+reactionCount = [0,0]
 
-#回したらどのくらい反応させたままにするか
-reactionCountMax = 1500
-#以下がreactionCountMaxを超えたら皿の反応がなくなる
-reactionCount = 0
+# 回転フラグ
+upRot = [0,0]
+downRot = [0,0]
+finalRotUp = [0,0]
 
-#回転フラグ
-upRot = 0
-downRot = 0
-finalRotUp = 0
+# 一旦止めないと再入力できないようにするための変数
+rotAfterTheEndStop = [1,1]
 
-#一旦止めないと再入力できないようにするための変数
-rotAfterTheEndStop = 1
-
-#短時間で同一方向に入力できないようにする変数
-#入力後に以下の変数の加算が開始し、notCountChangeTime == notCountChangeTimeMaxになったら同じ方向への入力が可能になる
-notCountChangeTime = 0
-notCountChangeTimeMax = 3500
+# 短時間で同一方向に入力できないようにする変数
+# 入力後に以下の変数の加算が開始し、notCountChangeTime == NOT_COUNT_CHANGE_TIME_MAXになったら同じ方向への入力が可能になる
+notCountChangeTime = [0,0]
+NOT_COUNT_CHANGE_TIME_MAX = 3500
     
-def checkButton():
+
+# IIDXモード
+# アーケード版IIDXを再現
+
+# EZ2ONモード
+# コントローラーをEZ2ONで使えるようにするモード
+# 皿を回してすぐに反対側に回したら数msだけ皿の反応を無くし、連続入力を可能にする
+# EZ2ONは片方のキー、ボタンを押している間にもう片方のキー、ボタンを押しても反応しない(このコントローラーを使う場合はキー、ボタン = 皿で、皿を回している間に反対側に回しても反応しない)ので、上記の処理が必要
+    
+MODE_IIDX = 0
+MODE_EZ2ON = 1
+MODE_MAX = MODE_EZ2ON
+currentMode = MODE_IIDX
+prePushModeButton = 0
+modeChangeButton = digitalio.DigitalInOut(GP27)
+modeChangeButton.direction = digitalio.Direction.INPUT
+modeChangeButton.pull = digitalio.Pull.UP
+
+def changeMode():
+    # 押された瞬間モード切替
+        if modeChangeButton == True and prePushModeButton == 0:
+            currentMode = currentMode + 1
+            prePushModeButton = 1
+            if currentMode == MODE_MAX:
+                currentMode = 0
+        else:
+            prePushModeButton = 0
+            
+    
+# プレイヤー番号は0スタート
+def checkButton(playerNum):
     ev = keys.events.get()
     if ev is not None:
-        key = keys1P[ev.key_number]
+        key = keyCodes[playerNum][ev.key_number + playerNum * 11 ]
         if ev.pressed:
             kbd.press(key)
         else:
@@ -122,11 +160,11 @@ def checkButton():
 
  
 
-def checkEncoder():
+def checkEncoder(playerNum):
     global prePos,preCount,count,reactionCount,upRot,downRot,continuousRotCount,finalRotUp,rotAfterTheEndStop,stopAfterTheEndRot,notCountChangeTime
-    pos = encoder.position
-    cPos = pos - prePos
-    moveDir = 0#1下 -1上 0未回転 
+    pos = encoder[playerNum].position
+    cPos = pos - prePos[playerNum]
+    moveDir = 0# 1下 -1上 0未回転 
     if cPos > 0:
         moveDir = 1
     elif cPos < 0:
@@ -134,74 +172,81 @@ def checkEncoder():
     else:
         moveDir = 0
           
-    #上回転
+    # 上回転
     if moveDir == -1: 
-        if count > 0:#プラスだったら0を代入
-            count = 0
-        count = count - 1
-        if count < -countMax:
-            reactionCount = 0#回したら反応時間をリセット
+        if count[playerNum] > 0:# プラスだったら0を代入
+            count[playerNum] = 0
+        count[playerNum] = count[playerNum] - 1
+        if count[playerNum] < -COUNT_MAX:
+            reactionCount[playerNum] = 0# 回したら反応時間をリセット
         
-        if count < -countMax and rotAfterTheEndStop == 1 and finalRotUp == 1 or finalRotUp == 0 and count < -countMax:#条件満たしたら入力
-            downRot = 0 #逆に回転していたら中止
-            kbd.press(scrUp1P)
-            kbd.release(scrDown1P)
-            upRot = 1
-            count = 0
-            finalRotUp = 1
-            rotAfterTheEndStop = 0
-            notCountChangeTime = 0
-    #下回転
+        if count[playerNum] < -COUNT_MAX and rotAfterTheEndStop[playerNum] == 1 and finalRotUp[playerNum] == 1 or finalRotUp[playerNum] == 0 and count[playerNum] < -COUNT_MAX:# 条件満たしたら入力
+            downRot[playerNum] = 0 # 逆に回転していたら中止
+            kbd.press(scrUp[playerNum])
+            kbd.release(scrDown[playerNum])
+            upRot[playerNum] = 1
+            count[playerNum] = 0
+            finalRotUp[playerNum] = 1
+            rotAfterTheEndStop[playerNum] = 0
+            notCountChangeTime[playerNum] = 0
+    # 下回転
     elif moveDir == 1:
-        if count < 0:#マイナスだったら0を代入
-            count = 0
-        count = count + 1
-        if count > countMax:
-            reactionCount = 0#回したら反応時間をリセット
+        if count[playerNum] < 0:# マイナスだったら0を代入
+            count[playerNum] = 0
+        count[playerNum] = count[playerNum] + 1
+        if count[playerNum] > COUNT_MAX:
+            reactionCount[playerNum] = 0# 回したら反応時間をリセット
             
-        if count > countMax and rotAfterTheEndStop == 1 and finalRotUp == 0 or finalRotUp == 1 and count > countMax:
-            upRot = 0 #逆に回転していたら中止
-            kbd.press(scrDown1P)
-            kbd.release(scrUp1P)
-            downRot = 1
-            count = 0
-            finalRotUp = 0
-            rotAfterTheEndStop = 0
-            notCountChangeTime = 0
-            reactionCount = 0#回したら反応時間をリセット
+        if count[playerNum] > COUNT_MAX and rotAfterTheEndStop[playerNum] == 1 and finalRotUp[playerNum] == 0 or finalRotUp[playerNum] == 1 and count[playerNum] > COUNT_MAX:
+            upRot[playerNum] = 0 # 逆に回転していたら中止
+            kbd.press(scrDown[playerNum])
+            kbd.release(scrUp[playerNum])
+            downRot[playerNum] = 1
+            count[playerNum] = 0
+            finalRotUp[playerNum] = 0
+            rotAfterTheEndStop[playerNum] = 0
+            notCountChangeTime[playerNum] = 0
+            reactionCount[playerNum] = 0# 回したら反応時間をリセット
     
-    prePos = pos
+    prePos[playerNum] = pos
     
-    if preCount == count:
-        notCountChangeTime = notCountChangeTime + 1
-        if notCountChangeTime == notCountChangeTimeMax:
-            notCountChangeTime = notCountChangeTimeMax
-            rotAfterTheEndStop = 1
-            count = 0
+    if preCount[playerNum] == count[playerNum]:
+        notCountChangeTime[playerNum] = notCountChangeTime[playerNum] + 1
+        if notCountChangeTime[playerNum] == NOT_COUNT_CHANGE_TIME_MAX:
+            notCountChangeTime[playerNum] = NOT_COUNT_CHANGE_TIME_MAX
+            rotAfterTheEndStop[playerNum] = 1
+            count[playerNum] = 0
             
-    preCount = count
+    preCount[playerNum] = count
      
-    #回転時間加算と回転入力終了処理
-    if upRot == 1:
-        reactionCount = reactionCount + 1
-        if reactionCount == reactionCountMax:#一定時間経ったら回転中止
-            reactionCount = 0
-            upRot = 0
-            count = 0
-            kbd.release(scrUp1P)
-            continuousRotCount = 0
+    # 回転時間加算と回転入力終了処理
+    if upRot[playerNum] == 1:
+        reactionCount[playerNum] = reactionCount[playerNum] + 1
+        if reactionCount[playerNum] == REACTION_COUNT_MAX:# 一定時間経ったら回転中止
+            reactionCount[playerNum] = 0
+            upRot[playerNum] = 0
+            count[playerNum] = 0
+            kbd.release(scrUp[playerNum])
+            continuousRotCount[playerNum] = 0
             
-    elif downRot == 1:
-        reactionCount = reactionCount + 1
-        if reactionCount == reactionCountMax:#一定時間経ったら回転中止
-            reactionCount = 0
-            downRot = 0
-            count = 0
-            kbd.release(scrDown1P)
-            continuousRotCount = 0
+    elif downRot[playerNum] == 1:
+        reactionCount[playerNum] = reactionCountv + 1
+        if reactionCount[playerNum] == REACTION_COUNT_MAX:# 一定時間経ったら回転中止
+            reactionCount[playerNum] = 0
+            downRot[playerNum] = 0
+            count[playerNum] = 0
+            kbd.release(scrDown[playerNum])
+            continuousRotCount[playerNum] = 0
             
         
 while True:
-    #2Pの処理を書く際は、キー以外の変数を配列にし、引数で配列番号を指定して処理すること
-     checkEncoder()
-     checkButton()
+    # 1P処理
+     checkEncoder(0)
+     checkButton(0)
+     
+     # 2P処理
+     checkEncoder(1)
+     checkButton(1)
+     
+     # モードの切替
+     changeMode()
